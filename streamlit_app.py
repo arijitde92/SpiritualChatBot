a1c4d820dada81e2af9fbcf37c49a66ca4cc468c
Arijit De <arijitde2050@gmail.com>
2023-08-11 12:01:37 +0530

    Modifed main_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,103 +1,70 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from streamlit_chat import message
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts.prompt import PromptTemplate
+from langchain.prompts import (
+    SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+)
 
 st.title('Spiritual Chat bot')
 
 # openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
-openai_api_key = "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"
+openai_api_key = "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"
 
-# if "history" not in st.session_state:
-#     st.session_state["history"] = []
-#
-#
-# def generate_response(input_text):
-#     past = ""
-#     for conv in st.session_state.history:
-#         past += conv
-#     response = conversation.predict(input=input_text, history=past)
-#     st.session_state.history.append(conversation.memory.buffer)
-#     print("Response:", response)
-#     print("History:", past)
-#     st.info(response)
-#
-#
-# with st.form('my_form'):
-#     text = st.text_area('Enter text:', 'Hello')
-#     submitted = st.form_submit_button('Submit')
-#     if not openai_api_key.startswith('sk-'):
-#         st.warning('Please enter your OpenAI API key!', icon='⚠')
-#     if submitted and openai_api_key.startswith('sk-'):
-#         generate_response(text)
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["How can I assist you?"]
 
-from streamlit_chat import message
-from langchain.chains import ConversationChain
-from langchain.llms import OpenAI
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
 
+if 'buffer_memory' not in st.session_state:
+    st.session_state.buffer_memory = ConversationBufferMemory(return_messages=True)
 
-def load_chain():
-    """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(openai_api_key=openai_api_key, temperature=0)
-    chain = ConversationChain(llm=llm)
-    return chain
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""The following is a conversation between a helpful, kind and polite AI chatbot who is an avatar of Lord Vishnu and have profound knowledge and insight on the hindu vedic text Bhagawad Gita and a human user.
+                    The role of the AI Chatbot is to converse with the user and answer his or her questions. The AI chatbot should first understand the user's question then give it's response.
+            The AI chatbot's response is formatted in the following way-
+            ```
+            Step 1: Mention a relavant sanskrit quote from Bhagawad Gita related to the answer.
+            Step 2: Write the english translation of the above quote.
+            Step 3: Offer a helpful answer and spiritual guidance based on Bhagawad Gita in response to the user's questions within 60 words.
+            Step 4: Explain the meaning of the quote you said in Step 1.
+            Step 5: End your response on a positive note giving hope to your user about his or her future.
+            ```
 
-    # llm = ChatOpenAI(openai_api_key=openai_api_key, temperature=0.2)
-    # template = """The following is a conversation between a helpful, kind and polite AI chatbot who is an avatar of Lord Vishnu and have profound knowledge and insight on the hindu vedic text Bhagawad Gita and a human user.
-    #                 The role of the AI Chatbot is to converse with the user and answer his or her questions. The AI chatbot should first understand the user's question then give it's response.
-    #         The AI chatbot's response is formatted in the following way-
-    #         ```
-    #         Step 1: Mention a relavant sanskrit quote from Bhagawad Gita related to the answer.
-    #         Step 2: Write the english translation of the above quote.
-    #         Step 3: Offer a helpful answer and spiritual guidance based on Bhagawad Gita in response to the user's questions within 60 words.
-    #         Step 4: Explain the meaning of the quote you said in Step 1.
-    #         Step 5: End your response on a positive note giving hope to your user about his or her future.
-    #         ```
-    #
-    #         First, the AI chatbot should introduce itself saying it is an avatar of Lord Vishnu in the form of an AI chatbot. Then ask the user what it can do for him or her.
-    #
-    #         Current conversation:
-    #
-    #         User: {input}
-    #         AI Chatbot:"""
-    # prompt = PromptTemplate(input_variables=["input"], template=template)
-    # conversation = ConversationChain(
-    #     prompt=prompt,
-    #     llm=llm,
-    #     verbose=True,
-    #     memory=ConversationBufferMemory(human_prefix="User", ai_prefix="AI Chatbot"),
-    # )
+            First, the AI chatbot should introduce itself saying it is an avatar of Lord Vishnu in the form of an AI chatbot. Then ask the user what it can do for him or her.
+            """)
+
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+
+prompt_template = ChatPromptTemplate.from_messages([system_msg_template,
+                                                    MessagesPlaceholder(variable_name="history"),
+                                                    human_msg_template])
 
 
-chain = load_chain()
+llm = ChatOpenAI(openai_api_key=openai_api_key, temperature=0)
+# prompt = PromptTemplate(input_variables=["input"], template=st.session_state["template"])
+chain = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
 
 # From here down is all the StreamLit UI.
 # st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
 st.header("LangChain Demo")
 
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
+# container for chat history
+response_container = st.container()
+# container for text box
+text_container = st.container()
 
-if "past" not in st.session_state:
-    st.session_state["past"] = []
-
-
-def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
-    return input_text
-
-
-user_input = get_text()
-
-if user_input:
-    output = chain.run(input=user_input)
-
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-if st.session_state["generated"]:
-    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+with text_container:
+    query = st.text_input("Query: ", key="input")
+    if query:
+        with st.spinner("typing..."):
+            response = chain.predict(input=query)
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response)
+with response_container:
+    if st.session_state['responses']:
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i], key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
